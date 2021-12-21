c2e920173c8f0d8bad996b9af066b626b5cfc2d1
howdood <68200851+howdood@users.noreply.github.com>
2021-12-21 16:28:57 +0000

    Create IPsaverPI.js

---
+++ IPsaverPI.js
@@ -0,0 +1,32 @@
+//run this under Google Apps Script to provide a remote host for your server's local IP address 
+function doPost(e) {
+  var cache = CacheService.getScriptCache();
+  if (e) {
+    var json = JSON.parse(e.postData.contents);
+    if(json.addr) {
+      Logger.log("sent IP");
+      //if it's sent in a new IP address, log that with a 24hr expiration
+      var addr = JSON.parse(e.postData.contents).addr;
+      cache.put("ipCache", addr, (60*60*24));
+      return ContentService
+      .createTextOutput(JSON.stringify({
+        "result": "success",
+        "response": "ip saved as " + addr
+      }))
+      .setMimeType(ContentService.MimeType.JSON);
+    } else {
+      //send back the current IP
+      var ipaddr = cache.get("ipCache");
+      return ContentService
+      .createTextOutput(JSON.stringify({
+      "ip": ipaddr
+      }))
+    .setMimeType(ContentService.MimeType.JSON);
+    }
+  } else {
+    Logger.log("requested ip");
+    //send back the current IP
+    var ipaddr = cache.get("ipCache");
+    Logger.log(ipaddr);
+  }
+}
